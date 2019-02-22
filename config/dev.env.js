3cf8db38518fe2cf3adf2b8890defe041ff2cb1e
Nandiheath <nandi0315@gmail.com>
2019-02-23 01:47:20 +0800

    clean up

--- config/dev.env.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-const merge = require('webpack-merge')
-const prodEnv = require('./prod.env')
-module.exports = merge(prodEnv, {
-  BOARDNAME: '"peanuts_organiser"',
-  NODE_ENV: '"development"'
-})
